76657c0d20b85673b24664f083118d836af375b8
TimmieO <timmieoskarsson01@gmail.com>
2021-11-15 19:39:11 +0100

    Added list and actions

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import submitHelper from "../../helper/submitHelper";
 import checkAccess from "../../helper/userHasAccess"
+import performAction from "../../helper/perfAction"
 import fetchPageData from "../../helper/fetchPageDataHelper"
+import { AiFillEdit, AiFillDelete } from "react-icons/ai"
+import "./admin.css";
+
 export default function Admin() {
 
   const [fetchedData, setFetchedData] = useState([]);
@@ -16,6 +20,15 @@ export default function Admin() {
     console.log(fetchedData);
   }
 
+  const editTableRow = async(rowId) => {
+    console.log(rowId);
+  }
+
+  const removeUser = async(userId) => {
+    console.log(userId);
+    let status = await performAction(userId, "delete");
+    console.log(performAction(status))
+  }
   /*
 
    */
@@ -25,19 +38,30 @@ export default function Admin() {
       <h1>Admin page</h1>
       <table className="admin-table">
         <thead>
-        <tr>
-          <th>id</th>
-          <th>username</th>
-          <th>email</th>
-        </tr>
+          <tr>
+            <th>id</th>
+            <th>username</th>
+            <th>firstname</th>
+            <th>lastname</th>
+            <th>email</th>
+            <th>level</th>
+            <th>Action</th>
+          </tr>
         </thead>
       {fetchedData.map((user, index) => (
         <tbody>
-        <tr>
-          <td>{user.user_id}</td>
-          <td>{user.username}</td>
-          <td>{user.email}</td>
-        </tr>
+          <tr>
+            <td>{user.user_id}</td>
+            <td>{user.username}</td>
+            <td>{user.firstname}</td>
+            <td>{user.lastname}</td>
+            <td>{user.email}</td>
+            <td>{user.access_level}</td>
+            <td>
+              <AiFillEdit onClick={() => editTableRow(index)}/>
+              <AiFillDelete onClick={() => removeUser(user.user_id)}/>
+            </td>
+          </tr>
         </tbody>
       ))}
       </table>
