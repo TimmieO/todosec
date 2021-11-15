4c03fb3aa946621823fe41d376eb79b8b8e84489
TimmieO <timmieoskarsson01@gmail.com>
2021-11-15 20:28:30 +0100

    added edit for accounts

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -3,12 +3,15 @@ import submitHelper from "../../helper/submitHelper";
 import checkAccess from "../../helper/userHasAccess"
 import performAction from "../../helper/perfAction"
 import fetchPageData from "../../helper/fetchPageDataHelper"
-import { AiFillEdit, AiFillDelete } from "react-icons/ai"
+import { AiFillEdit, AiFillDelete, AiOutlineClose, AiOutlineCheck } from "react-icons/ai"
 import "./admin.css";
 
 export default function Admin() {
 
   const [fetchedData, setFetchedData] = useState([]);
+
+  const [activeRowEdit, setActiveRowEdit] = useState();
+  const [editData, setEditData] = useState({username: null, firstname: null, lastname: null, email: null, level: null, });
 
   useEffect(() => {
     getPageData();
@@ -20,9 +23,49 @@ export default function Admin() {
     console.log(fetchedData);
   }
 
-  const editTableRow = async(rowId) => {
-    console.log(rowId);
+  const editTableRow = async(rowId, action) => {
+
+    switch(action){
+      case "start":
+      {
+        startEdit(rowId);
+        break;
+      }
+      case "cancel":
+      {
+        cancelEdit(rowId);
+        break;
+      }
+      case "submit":
+      {
+        submitEdit(rowId);
+        break;
+      }
+
+    }
+
+    //Finish this
+    function startEdit(rowId){
+      setActiveRowEdit(rowId)
+      setEditData(fetchedData[rowId])
+    }
+    function cancelEdit(rowId){
+      setActiveRowEdit(null)
+    }
+    function submitEdit(rowId){
+
+    }
   }
+
+  //Change text in state onChange
+  const onChangeUpdateStateText = async (e) => {
+    let inpType = e.target.dataset.input;
+    let inpVal = e.target.value;
+    setEditData((prevState) => ({
+      ...prevState,
+      [inpType]: inpVal,
+    }));
+  };
 
   const removeUser = async(userId) => {
     console.log(userId);
@@ -50,7 +93,7 @@ export default function Admin() {
         </thead>
       {fetchedData.map((user, index) => (
         <tbody>
-          <tr>
+          <tr className={activeRowEdit == index ? "hidden" : null}>
             <td>{user.user_id}</td>
             <td>{user.username}</td>
             <td>{user.firstname}</td>
@@ -58,8 +101,20 @@ export default function Admin() {
             <td>{user.email}</td>
             <td>{user.access_level}</td>
             <td>
-              <AiFillEdit onClick={() => editTableRow(index)}/>
-              <AiFillDelete onClick={() => removeUser(user.user_id)}/>
+              <AiFillEdit className="admin-table-action-icon" onClick={() => editTableRow(index, "start")}/>
+              <AiFillDelete className="admin-table-action-icon" onClick={() => removeUser(user.user_id)}/>
+            </td>
+          </tr>
+          <tr className={activeRowEdit == index ? null : "hidden"}>
+            <td>{user.user_id}</td>
+            <td><input type="text" data-input="username" value={editData.username} onChange={onChangeUpdateStateText} /></td>
+            <td><input type="text" data-input="firstname" value={editData.firstname} onChange={onChangeUpdateStateText} /></td>
+            <td><input type="text" data-input="lastname" value={editData.lastname} onChange={onChangeUpdateStateText} /></td>
+            <td><input type="text" data-input="email" value={editData.email} onChange={onChangeUpdateStateText} /></td>
+            <td><input type="text" data-input="access_level" value={editData.access_level} onChange={onChangeUpdateStateText} /></td>
+            <td>
+              <AiOutlineCheck className="admin-table-action-icon" onClick={() => editTableRow(index, "submit")}/>
+              <AiOutlineClose className="admin-table-action-icon" onClick={() => editTableRow(index, "cancel")}/>
             </td>
           </tr>
         </tbody>
